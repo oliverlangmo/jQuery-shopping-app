904bd0c7a0d34d08a1a38e19ffc965ec923cccca
oliverlangmo <oliver.langmo@gmail.com>
2015-01-10 22:37:25 -0600

    whiteboard

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 $(document).ready(function() {
-		$("#add-item").click(function(){
-			if( $('input#input').val().trim().length === 0 ) {
-				$('#error').show();
-				$('input#input').val("");
+	$("#add-item").click(function(){
+		if( $('input#input').val().trim().length === 0 ) {
+			$('#error').show();
+			$('input#input').val("");
 		} else {
 			$("ol").append("<li class='need'><input type=checkbox>" + $("input#input").val() + "</li>");
 			$("input#input").val('');
 			$('#error').hide();
-        }
-        $("#remove").on('click',function() {
-	      $('ol li:last-child').remove();
-	    })  
-});
-$("ol").on('click', '[type=checkbox]', function() {
-			$(this).closest("li").toggleClass("in-cart");	
-		})		
+		}
+	});
+	$("#remove").on('click',function() {
+	console.log('remove item');
+	$('ol li:last-child').remove();
+    });
+
+	$("ol").on('click', '[type=checkbox]', function() {
+		$(this).closest("li").toggleClass("in-cart");	
+	})		
 });
 
+  
